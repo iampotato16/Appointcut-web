9562a9647eb85f8d16dfb7de5a46e0b65f45398a
enairu <enairu62@gmail.com>
2021-10-21 14:01:47 +0800

    Buttons are working, request needs processings

--- server/routes/routesCustomers.js
+++ server/routes/routesCustomers.js
@@ -18,7 +18,10 @@ router.route('/')
         console.log("Received post request at customerPost with request: ", [addInput]);
         res.redirect('/customers');
     })
-
+router.post('/edit',(req,res)=>{
+    const reqString = JSON.stringify(req.body);
+    res.send(`Edit post receieved with request ${reqString}`)
+})
 
 module.exports = router;
 
